2f1cafbaeb0a66e8861222369ab5dc002a9e1fec
Ris-Otto <kasper.renlund@abo.fi>
2021-08-26 12:47:05 +0300

    Added PurpleBehaviour

---
+++ Assets/Scripts/Enemies/PurpleBehaviour.cs
@@ -0,0 +1,62 @@
+using Helpers;
+using Projectiles;
+using UnityEngine;
+
+namespace Enemies
+{
+    public class PurpleBehaviour : AbstractEnemy
+    {
+        private float _distanceTravelled;
+        private int _waypointIdx;
+        private ActiveObjectsTracker _et;
+        private Projectile lastProjectile;
+        public Enemy enemy;
+        private Vector3 _spawnOffset;
+        private float _timeToSave;
+
+        private new void Awake() {
+            base.Awake();
+        }
+        
+        public override bool IsAppropriateDamageType(Projectile projectile) {
+            bool toReturn = DamageType.CompareTo(projectile.DamageType) != 0;
+            if(!toReturn) projectile.ResetProjectileFromEnemy();
+            return toReturn;
+        }
+
+        #region getters/setters
+
+        protected override Projectile LastProjectile {
+            get => lastProjectile;
+            set => lastProjectile = value;
+        }
+
+        public override float distanceTravelled { 
+            get => _distanceTravelled;
+            protected set => _distanceTravelled = value; 
+        }
+
+        protected override int waypointIdx { get => _waypointIdx; set => _waypointIdx = value; }
+
+        protected override ActiveObjectsTracker et {
+            get => _et;
+            set => _et = value;
+        }
+
+
+        public override Enemy Enemy => enemy;
+        protected override ScriptableDamageType DamageType => enemy.damageType;
+
+        protected override float timeToSave {
+            get => _timeToSave;
+            set => _timeToSave = value;
+        }
+        
+        protected override Vector3 savedPos {
+            get => _spawnOffset;
+            set => _spawnOffset = value;
+        }
+        
+        #endregion
+    }
+}

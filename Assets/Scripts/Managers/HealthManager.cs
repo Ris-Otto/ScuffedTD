e0d861cea4654230f770848bb09450875286f4e8
Ris-Otto <kasper.renlund@abo.fi>
2022-04-07 09:11:28 +0300

    simple health system

---
+++ Assets/Scripts/Managers/HealthManager.cs
@@ -0,0 +1,50 @@
+using Enemies;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Managers
+{
+    public class HealthManager : MonoBehaviour, ILoggable
+    {
+
+        private int _health;
+        private int _healthLastRound;
+
+        private Log _log;
+
+        public static HealthManager Instance;
+
+        private UIManager ui;
+
+        private void Awake() {
+            if (Instance == null) Instance = this;
+            _health = 100;
+            _healthLastRound = _health;
+            ui = GameObject.FindGameObjectWithTag("Canvas").GetComponent<UIManager>();
+            ui.DisplayHealth(_health);
+        }
+
+        public void AddLogger() {
+            _log = Managers.Log.Instance;
+        }
+        
+        private void Update() {
+            if (_health > 0) return;
+            _log.Logger.Log(LogType.Log, "Game over");
+            _log.DisableLogger();
+            SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+        }
+
+        public void OnEnemyPassedThrough(AbstractEnemy enemy) {
+            _health -= enemy.Enemy.totalHealth;
+            ui.DisplayHealth(_health);
+        }
+
+        public void Log() {
+            int loss = _healthLastRound - _health;
+            if (loss == 0) return;
+            _log.Logger.Log(LogType.Log, $"0: Lost {loss} hit points, current hit points: {_health}");
+            _healthLastRound = _health;
+        }
+    }
+}
